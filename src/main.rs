b9c776fe99be401c0bf4bcb2abae9140b8d82041
Valenstain <svs.laboratory@gmail.com>
2024-06-18 17:44:38 +0300

    Add rand number

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-use std::io;
+use std::{cmp::Ordering, io};
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     println!("Please input your guess.");
+    println!("The secret number is {secret_number}");
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Filed to read line");
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Filed to read line");
+
+    let guess:u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("You win!"),
+        Ordering::Greater => println!("Too big"),
+    }
 }
